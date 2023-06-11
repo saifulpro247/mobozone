f48619db1b8729fccf4d8b20336490b8c889070e
Md. Saiful <saifulpro247@gmail.com>
2023-06-12 03:40:31 +0600

    this is

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+(function($) {
+    "use strict";
+
+
+      $(".slider").owlCarousel({
+        items: 1,
+        loop:true,
+        nav:true,
+        navText: ["<i class='bi bi-arrow-left-short'></i>", "<i class='bi bi-arrow-right-short'></i>"],
+        dots: true,
+      });
+
+    
+
+})($);
