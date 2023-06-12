013ecb3b16899f72eacd94aa6cc183c621edb99b
Md. Saiful <saifulpro247@gmail.com>
2023-06-12 23:54:55 +0600

    this is the main site

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,11 @@
         nav:true,
         navText: ["<i class='bi bi-arrow-left-short'></i>", "<i class='bi bi-arrow-right-short'></i>"],
         dots: true,
+        responsive:{
+        576:{
+            nav:false,
+        }
+        }
       });
 
     
